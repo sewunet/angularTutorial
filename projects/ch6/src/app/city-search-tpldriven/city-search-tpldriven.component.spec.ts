0a89390741eb15c97e173aa5483228669766e6a5
Sewunet Abebaw <sewunet92@gmail.com>
2021-10-21 17:49:54 +0300

    first commit

---
+++ projects/ch6/src/app/city-search-tpldriven/city-search-tpldriven.component.spec.ts
@@ -0,0 +1,53 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
+import { FormsModule } from '@angular/forms'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
+import {
+  ObservablePropertyStrategy,
+  autoSpyObj,
+  injectSpy,
+} from 'angular-unit-test-helper'
+import { of } from 'rxjs'
+
+import { MaterialModule } from '../material.module'
+import { WeatherService } from '../weather/weather.service'
+import { CitySearchTpldrivenComponent } from './city-search-tpldriven.component'
+
+describe('CitySearchTpldrivenComponent', () => {
+  let component: CitySearchTpldrivenComponent
+  let fixture: ComponentFixture<CitySearchTpldrivenComponent>
+  let weatherServiceMock: jasmine.SpyObj<WeatherService>
+
+  beforeEach(
+    waitForAsync(() => {
+      const weatherServiceSpy = autoSpyObj(
+        WeatherService,
+        ['currentWeather$'],
+        ObservablePropertyStrategy.BehaviorSubject
+      )
+
+      TestBed.configureTestingModule({
+        declarations: [CitySearchTpldrivenComponent],
+        providers: [{ provide: WeatherService, useValue: weatherServiceSpy }],
+        imports: [FormsModule, MaterialModule, NoopAnimationsModule],
+      }).compileComponents()
+
+      weatherServiceMock = injectSpy(WeatherService)
+    })
+  )
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CitySearchTpldrivenComponent)
+    component = fixture.componentInstance
+  })
+
+  it('should create', () => {
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of())
+
+    // Act
+    fixture.detectChanges() // triggers ngOnInit
+
+    // Assert
+    expect(component).toBeTruthy()
+  })
+})
---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,37 @@
+import { TestBed, waitForAsync } from '@angular/core/testing'
+import { createComponentMock } from 'angular-unit-test-helper'
+
+import { AppMaterialModule } from './app-material.module'
+import { AppComponent } from './app.component'
+
+describe('AppComponent', () => {
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          AppComponent,
+          createComponentMock('CurrentWeatherComponent'),
+          createComponentMock('CitySearchComponent'),
+        ],
+        imports: [AppMaterialModule],
+      }).compileComponents()
+    })
+  )
+  it(
+    'should create the app',
+    waitForAsync(() => {
+      const fixture = TestBed.createComponent(AppComponent)
+      const app = fixture.debugElement.componentInstance
+      expect(app).toBeTruthy()
+    })
+  )
+  it(
+    'should render title in a h1 tag',
+    waitForAsync(() => {
+      const fixture = TestBed.createComponent(AppComponent)
+      fixture.detectChanges()
+      const compiled = fixture.debugElement.nativeElement
+      expect(compiled.querySelector('span').textContent).toContain('LocalCast Weather')
+    })
+  )
+})
